37b1826bd55a29193467aaacf8e44affa1db6dd7
Mona <mona@Monas-MacBook-Air.local>
2022-06-28 18:59:41 +0430

    add test

--- main_test.go
+++ main_test.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"testing"
-	"user-management/controllers"
 )
 
 func TestGetAllUsers(t *testing.T) {
-	req, err := http.NewRequest("GET", "/users", nil)
-	defer req.Body.Close()
+
+	_, err := http.NewRequest("GET", "/users/uses", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(controllers.GetAllUsers)
-	handler.ServeHTTP(rr, req)
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
 }
